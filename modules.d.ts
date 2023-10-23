963f1e2ddf54dd28f842dd327e3cbc480beddd20
Wei Jie <Wei_Jie_Phua_FROM.TP@mom.gov.sg>
2023-10-23 10:22:49 +0800

    added 9Plugin-FormComponent

--- modules.d.ts
+++ modules.d.ts
@@ -1,1 +1,2 @@
 declare module '@momwins/mom-design-system-v3';
+declare module '@momwins/wins-common-form-component';
--- plugins/momDesignSystemV3.client.js
+++ plugins/momDesignSystemV3.client.js
@@ -1,5 +1,10 @@
 import DesignSystem from '@momwins/mom-design-system-v3';
+import {CommonFormComponent} from '@momwins/wins-common-form-component';
 export default defineNuxtPlugin(nuxtApp => {
     console.log("🚀 ~ file: momDesignSystemV3.js:2 ~ nuxtApp:", nuxtApp)
     nuxtApp.vueApp.use(DesignSystem)
+    nuxtApp.vueApp.use(CommonFormComponent, {
+    lang: 'en',
+    baseUrl: '/workpass/ep',
+  });
 })
